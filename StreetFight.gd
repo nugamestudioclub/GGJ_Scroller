39b88d48bc5786baeb14a86041d28fc532a0f817
Leo Norton <leoznorton@gmail.com>
2024-01-27 14:50:30 -0500

    More work on StreetFight (added inputs)

--- StreetFight.gd
+++ StreetFight.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var player
 var opponent
+enum STStates {Idle, Attacking, Blocking, Damaged}
+var _state : int = STStates.Idle
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
@@ -10,7 +12,20 @@ func _ready():
 	punch_routine()
 
 func _process(delta):
-	pass
+	#attacking if player is idle
+	if Input.is_action_pressed("ui_up") && _state == STStates.Idle:
+		_state = STStates.Attacking
+		player.position.y -= 50
+		await get_tree().create_timer(.3).timeout
+		player.position.y += 50
+		await get_tree().create_timer(.7).timeout
+		_state = STStates.Idle
+	if Input.is_action_pressed("ui_down") && _state == STStates.Idle:
+		_state = STStates.Blocking
+		player.rotation_degrees += 90
+		await get_tree().create_timer(.7).timeout
+		player.rotation_degrees -= 90
+		_state = STStates.Idle
 
 func punch_routine():
 	var waitTime = rng.randf_range(1.0, 5.0)
