0712792bf2f3d56a93e430ae0b9176f32dde40b3
Komo-Blows <constanti.n@northeastern.edu>
2024-01-26 20:46:14 -0500

    Inputs

--- script1.gd
+++ script1.gd
@@ -1,13 +1,19 @@
 extends Sprite2D
 
-
+var input = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+#How to do inputs for all your minigames!
+func inputs():
+	if Input.is_action_pressed("Up"):
+		position.y+=1
+	input.x = Input.get_action_strength("Left") - Input.get_action_strength("Right")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.new("Up")
-		position.x+=1
+	inputs()
+	
+	
 	
