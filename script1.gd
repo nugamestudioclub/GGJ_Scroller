72f8747cd32e712d9c931ebcaf328a96cb9fa637
Komo-Blows <constanti.n@northeastern.edu>
2024-01-27 15:31:46 -0500

    Adding Random Selection

--- script1.gd
+++ script1.gd
@@ -1,15 +1,21 @@
 extends Sprite2D
 
 var input = Vector2(0, 0)
+var funny_types = ["plat", "fight", "trex", "drive"]
+signal scroll(type)
+var last_picked = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 #How to do inputs for all your minigames!
 func inputs():
-	if Input.is_action_pressed("Up"):
-		position.y+=1
-	input.x = Input.get_action_strength("Left") - Input.get_action_strength("Right")
+	if Input.is_action_pressed("enter"):
+		var random = funny_types.pick_random()
+		while random == last_picked:
+			random = funny_types.pick_random()
+		scroll.emit(random)
+		last_picked = random
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
